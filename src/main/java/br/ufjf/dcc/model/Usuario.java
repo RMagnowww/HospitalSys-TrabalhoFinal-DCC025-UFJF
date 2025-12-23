2384071796d2bfed61369f04f43d07c43b0abb5c
Rafael Magno <rafaelmagnocampos@gmail.com>
2025-12-23 01:43:38 -0300

    Usuario: muda de abstract class para class. Cria construtor, getters e setters. Transforma nome e CPF em final

--- src/main/java/br/ufjf/dcc/model/Usuario.java
+++ src/main/java/br/ufjf/dcc/model/Usuario.java
@@ -1,9 +1,41 @@
 package br.ufjf.dcc.model;
 
-public abstract class Usuario {
-    private String nome;
-    private String cpf;
+public class Usuario {
+    private final String nome;
+    private final String cpf;
     private String senha;
     private String telefone;
     private String email;
+    
+    public Usuario(String nome, String cpf, String senha, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public String getCpf() {
+        return this.cpf;
+    }
+    public String getTelefone() {
+        return this.telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public string getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {          
+        return this.senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;     
+    }
 }
