10ffa215880536b70cba7b2de0a475dc9853ff53
Rafael Magno <rafaelmagnocampos@gmail.com>
2025-12-23 01:45:30 -0300

    Paciente: Adiciona atributo endereço. Cria construtor, getters e setters

--- src/main/java/br/ufjf/dcc/model/Paciente.java
+++ src/main/java/br/ufjf/dcc/model/Paciente.java
@@ -1,6 +1,16 @@
 package br.ufjf.dcc.model;
 
 public class Paciente extends Usuario{
+    private String endereco;
 
-
+    public Paciente(String nome, String cpf, String senha, String telefone, String email, String endereco) {
+        super(nome, cpf, senha, telefone, email);
+        this.endereco = endereco;
+    }
+    public String getEndereco() {
+        return this.endereco;
+    } 
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }   
 }
