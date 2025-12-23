6812ace0657f7dba4b7438103ff1650cc0bbaf6d
Rafael Magno <rafaelmagnocampos@gmail.com>
2025-12-23 01:09:23 -0300

    Consulta: Adiciona atributos e construtor

--- src/main/java/br/ufjf/dcc/model/Consulta.java
+++ src/main/java/br/ufjf/dcc/model/Consulta.java
@@ -1,4 +1,18 @@
 package br.ufjf.dcc.model;
 
 public class Consulta {
+    private String data;
+    private String horario;
+    private String descricao;
+    private Paciente paciente;
+    private Medico medico;
+
+    public Consulta(String data, String horario, String descricao, Paciente paciente, Medico medico) {
+        this.data = data;
+        this.horario = horario;
+        this.descricao = descricao;
+        this.paciente = paciente;
+        this.medico = medico;
+    }
+
 }
