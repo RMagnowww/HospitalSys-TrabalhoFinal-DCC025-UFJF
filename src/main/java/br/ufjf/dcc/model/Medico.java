f8c27465e67d5ccdadf3c50eff7682622e546b94
Rafael Magno <rafaelmagnocampos@gmail.com>
2025-12-23 01:46:22 -0300

    Medico: cria construtor

--- src/main/java/br/ufjf/dcc/model/Medico.java
+++ src/main/java/br/ufjf/dcc/model/Medico.java
@@ -1,4 +1,8 @@
 package br.ufjf.dcc.model;
 
 public class Medico extends Usuario {
+    
+    public Medico(String nome, String cpf, String email, String telefone, String senha){
+        super(nome, cpf, email, telefone, senha);
+    }
 }
