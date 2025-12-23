204c1fb24d1ed7d2662687fca75ea03e3d4d0a54
Rafael Magno <rafaelmagnocampos@gmail.com>
2025-12-23 01:47:03 -0300

    Endereco: Adiciona atributos cidade e numero. Cria construtor

--- src/main/java/br/ufjf/dcc/model/Endereco.java
+++ src/main/java/br/ufjf/dcc/model/Endereco.java
@@ -4,4 +4,14 @@ public class Endereco {
     private String rua;
     private String bairro;
     private String cep;
+    private String cidade;
+    private String numero;
+    
+    public Endereco(String rua, String bairro, String cep, String cidade, String numero) {
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.numero = numero;
+    }
 }
