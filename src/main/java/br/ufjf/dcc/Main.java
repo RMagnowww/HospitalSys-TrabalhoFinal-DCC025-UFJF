8c931ee70bdb6b813a21cacd073886a15bad5c0e
Rafael Magno <rafaelmagnocampos@gmail.com>
2025-12-25 13:24:48 -0300

    Muda Main para incializar tela de login

--- src/main/java/br/ufjf/dcc/Main.java
+++ src/main/java/br/ufjf/dcc/Main.java
@@ -1,7 +1,9 @@
 package br.ufjf.dcc;
+import br.ufjf.dcc.view.TelaLogin;
 
 public class Main{
     public static void main(String[] args){
-
+        TelaLogin telaLogin = new TelaLogin();
+        telaLogin.abrirLogin();
     }
 }
