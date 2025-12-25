dd9433924355da7fe934288852d4bfa62a0df94f
Rafael Magno Campos da Silva <rafael.magno@estudante.ufjf.br>
2025-12-25 16:57:28 -0300

    Cria tela de menu para pacientes

--- src/main/java/br/ufjf/dcc/view/TelaMenuPaciente.java
+++ src/main/java/br/ufjf/dcc/view/TelaMenuPaciente.java
@@ -1,4 +1,57 @@
 package br.ufjf.dcc.view;
+import javax.swing.*;
+import java.awt.*;
 
 public class TelaMenuPaciente {
+    private JFrame frame;
+    private JPanel painelPrincipal;
+    private JPanel painelEsq;
+    private JPanel painelDir;
+    private JButton agendaConsulta;
+    private JButton docsMedicos;
+    private JButton historico;
+    private JButton dadosPessoais;
+    private JButton verificaVisita;
+    private JButton botaoSair;
+
+    public TelaMenuPaciente() {
+        frame = new JFrame("Menu - Paciente");
+        painelPrincipal = new JPanel();
+        painelEsq = new JPanel(); 
+        painelDir = new JPanel();  
+        agendaConsulta = new JButton("Agendamento de Consultas");
+        docsMedicos = new JButton("Documentos Médicos");
+        historico = new JButton("Histórico Médico");
+        dadosPessoais = new JButton("Dados Pessoais");
+        verificaVisita = new JButton("Verificar Disponibilidade de Visita");
+        botaoSair = new JButton("Sair");
+    }
+
+    public void abrirMenuPaciente(){
+        frame.setSize(800,450);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setResizable(false);
+
+        botaoSair.addActionListener(e -> frame.dispose());
+
+        painelPrincipal.setLayout(new GridLayout(0, 2, 25, 0));
+        painelPrincipal.setBorder(BorderFactory.createEmptyBorder(45,35,45,35));
+
+        painelEsq.setLayout(new GridLayout(0,1,0,20));
+        painelEsq.add(agendaConsulta);
+        painelEsq.add(docsMedicos);
+        painelEsq.add(historico);
+
+        painelDir.setLayout(new GridLayout(0,1,0,20));
+        painelDir.add(dadosPessoais);
+        painelDir.add(verificaVisita);
+        painelDir.add(botaoSair);
+
+        painelPrincipal.add(painelEsq);
+        painelPrincipal.add(painelDir);
+
+        frame.add(painelPrincipal);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
 }
