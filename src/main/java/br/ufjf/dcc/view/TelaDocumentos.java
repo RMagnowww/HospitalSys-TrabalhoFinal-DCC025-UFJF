8c1f339149a3d9f465223a459c494374fb30d9eb
Rafael Magno Campos da Silva <rafael.magno@estudante.ufjf.br>
2025-12-27 00:52:57 -0300

    Adiciona painel Visualizador a TelaDocumentos e TelaHistorico

--- src/main/java/br/ufjf/dcc/view/TelaDocumentos.java
+++ src/main/java/br/ufjf/dcc/view/TelaDocumentos.java
@@ -11,7 +11,9 @@ public class TelaDocumentos{
     private JPanel painelInferior;
     private JList<Exame> listExames;
     private JList<Atestado> listAtestados;
+    private JTextPane painelVisualizar;
     private JButton botaoSair;
+    private JButton botaoAbrir;
 
     public TelaDocumentos(){
         frame = new JFrame("Documentos Médicos");
@@ -20,24 +22,31 @@ public class TelaDocumentos{
         painelInferior = new JPanel();
         listExames = new JList<Exame>();
         listAtestados = new JList<Atestado>();
+        painelVisualizar = new JTextPane();
         botaoSair = new JButton("Sair");
+        botaoAbrir = new JButton("Abrir");
     }
     public void abrirTelaDocumentos(){
-        frame.setSize(600,450);
+        frame.setSize(800,450);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         listExames.setBorder(BorderFactory.createTitledBorder("Resultados de Exames"));
         listAtestados.setBorder(BorderFactory.createTitledBorder("Atestados Médicos"));
+        painelVisualizar.setBorder(BorderFactory.createTitledBorder("Visualizador"));
+        painelVisualizar.setEditable(false);
+        painelVisualizar.setVisible(false);
         botaoSair.addActionListener(e -> frame.dispose());
 
-        painelSuperior.setLayout(new GridLayout(0,2,10,0));
+        painelSuperior.setLayout(new GridLayout(0,3,10,0));
         painelSuperior.add(new JScrollPane(listExames));
+        painelSuperior.add(painelVisualizar);
         painelSuperior.add(new JScrollPane(listAtestados));
 
-        painelInferior.setLayout(new BorderLayout(0,0));
+        painelInferior.setLayout(new GridLayout(0,2,10,0));
         painelInferior.setBorder(BorderFactory.createEmptyBorder(0,150,0,150));
         painelInferior.add(botaoSair);
+        painelInferior.add(botaoAbrir);
 
         painelPrincipal.setBorder(BorderFactory.createEmptyBorder(10,15,10,15));
         painelPrincipal.setLayout(new BorderLayout(5,5));
--- src/main/java/br/ufjf/dcc/view/TelaHistorico.java
+++ src/main/java/br/ufjf/dcc/view/TelaHistorico.java
@@ -6,29 +6,48 @@ import java.awt.*;
 public class TelaHistorico{
     private JFrame frame;
     private JPanel painelPrincipal;
+    private JPanel painelSuperior;
     private JPanel painelBotao;
     private JList<Consulta> listConsultas;
+    private JTextPane painelVisualizar;
     private JButton botaoSair;
+    private JButton botaoAbrir;
 
     public TelaHistorico(){
         frame = new JFrame("Histórico Médico");
         painelPrincipal = new JPanel();
+        painelSuperior = new JPanel();
         painelBotao = new JPanel();
         listConsultas = new JList<Consulta>();
+        painelVisualizar = new JTextPane();
         botaoSair = new JButton("Sair");
+        botaoAbrir = new JButton("Abrir");
     }
     public void abrirTelaHistorico(){
-        frame.setSize(400,500);
+        frame.setSize(600,500);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         listConsultas.setBorder(BorderFactory.createTitledBorder("Histórico de Consultas"));
+
         botaoSair.addActionListener(e -> frame.dispose());
+
+        painelBotao.setLayout(new GridLayout(0,2,10,0));
+        painelBotao.setBorder(BorderFactory.createEmptyBorder(0,120,0,120));
         painelBotao.add(botaoSair);
+        painelBotao.add(botaoAbrir);
+
+        painelVisualizar.setBorder(BorderFactory.createTitledBorder("Visualizador"));
+        painelVisualizar.setEditable(false);
+        painelVisualizar.setVisible(false);
+
+        painelSuperior.setLayout(new GridLayout(0,2,10,0));
+        painelSuperior.add(new JScrollPane(listConsultas));
+        painelSuperior.add(painelVisualizar);
 
         painelPrincipal.setLayout(new BorderLayout(10,10));
         painelPrincipal.setBorder(BorderFactory.createEmptyBorder(10,15,10,15));
-        painelPrincipal.add(new JScrollPane(listConsultas));
+        painelPrincipal.add(painelSuperior, BorderLayout.CENTER);
         painelPrincipal.add(painelBotao, BorderLayout.SOUTH);
 
         frame.add(painelPrincipal);
