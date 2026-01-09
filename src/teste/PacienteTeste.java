3fcbe7f6bbf6714605a3dc58c4a5c2e1bc903dc5
Gabriel Angelo Bovareto Batista <gabrielbatista.bovareto@estudante.ufjf.br>
2026-01-09 13:37:30 -0300

    Modifica Paciente Teste

--- src/teste/PacienteTeste.java
+++ src/teste/PacienteTeste.java
@@ -5,64 +5,70 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 class PacienteTest {
-     private static final String VALID_NAME = String.getInstance("Joao");
-     private static final String VALID_CPF = String.getInstance("086.756.245-90");
-     private static final String VALID_PASSWORD = String.getInstance("paciente123");
-     private static final String VALID_PHONE = String.getInstance("(32)96655-3322");
-     private static final String VALID_EMAIL = String.getInstance("joao@paciente.com");
+     private static final String VALID_NAME = "Joao";
+     private static final String VALID_CPF = "086.756.245-90";
+     private static final String VALID_PASSWORD = "paciente123";
+     private static final String VALID_PHONE = "(32)96655-3322";
+     private static final String VALID_EMAIL = "joao@paciente.com";
      private static final PerfilUsuario VALID_PROFILE = PerfilUsuario.PACIENTE;
-     private static final String VALID_DATADENACIMENTO = String.getInstance("07/04/1986");
-     private static final String VALID_TIPOSANGUINEO = String.getInstance("A+");
+     private static final String VALID_DATADENASCIMENTO = "07/04/1986";
+     private static final String VALID_TIPOSANGUINEO = "A+";
      private static final Endereco VALID_ENDERECO = Endereco.getInstance("Padre Cafe, 210 - Paineiras, Juiz de Fora - CEP: 78645-320");
 
+    private Paciente validPaciente;
      @BeforeEach
     void setUp() {
-        validPaciente = new Paciente(VALID_NAME, VALID_CPF, VALID_PHONE, VALID_EMAIL, VALID_PASSWORD, VALID_ENDERECO, VALID_DATADENACIMENTO, VALID_TIPOSANGUINEO);
+        validPaciente = new Paciente(VALID_NAME, VALID_CPF, VALID_PHONE, VALID_EMAIL, VALID_PASSWORD, VALID_ENDERECO, VALID_DATADENASCIMENTO, VALID_TIPOSANGUINEO);
     }
-      @Nested
-    @DisplayName("Datadenacimento Tests")
-     class DatadenacimentoTests{
-           @Test
-        void should_validate_datadenacimento_when_check_datadenacimento_of_paciente1() {
-            assertTrue(validUser.checkDatadenacimento("07/04/1986"));
-        }
+     @Test
+    void should_create_paciente_with_valid_data() {
+        assertEquals(VALID_NAME, validPaciente.getNome());
+        assertEquals(VALID_CPF, validPaciente.getCpf());
+        assertEquals(VALID_PHONE, validPaciente.getTelefone());
+        assertEquals(VALID_EMAIL, validPaciente.getEmail());
+        assertEquals(VALID_DATADENASCIMENTO, validPaciente.getDataNascimento());
+        assertEquals(VALID_TIPOSANGUINEO, validPaciente.getTipoSanguineo());
+        assertEquals(VALID_ENDERECO, validPaciente.getEndereco());
+    }
+    @Test
+    void should_throw_exception_when_dataNascimento_is_invalid() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Paciente(
+                VALID_NAME, VALID_CPF, VALID_PHONE, VALID_EMAIL, VALID_PASSWORD,
+                VALID_ENDERECO, "7/04/1986", VALID_TIPOSANGUINEO);
+        });
+    }
 
+    @Test
+    void should_throw_exception_when_tipoSanguineo_is_invalid() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Paciente(
+                VALID_NAME, VALID_CPF, VALID_PHONE, VALID_EMAIL, VALID_PASSWORD,
+                VALID_ENDERECO, VALID_DATADENASCIMENTO, "A+@");
+        });
+    }
+    @Nested
+@DisplayName("Usuario Tests")
+class UsuarioTests {
         @Test
-        void should_not_validate_datadenacimento_when_check_datadenacimento_of_paciente1() {
-            assertFalse(validUser.checkDatadenacimento("7/04/1986"));
-        }
-
+    void should_not_change_cpf_when_invalid() {
+        validPaciente.setCpf("086.756.45-90");
+        assertEquals("086.756.245-90", validPaciente.getCpf());
+    }
         @Test
-        void should_not_validate_null_datadenacimento() {
-            assertFalse(validUser.checkDatadenacimento(null));
-        }
-
+    void should_not_change_email_when_invalid() {
+        validPaciente.setEmail("renapaciente.com");
+        assertEquals(VALID_EMAIL, validPaciente.getEmail());
+    }
         @Test
-        void should_not_validate_empty_datadenacimento() {
-            assertFalse(validUser.checkDatadenacimento(""));
-        }
-     }
-           @Nested
-    @DisplayName("Tiposanguineo Tests")
-     class TiposanguineoTests{
-           @Test
-        void should_validate_Tiposanguineo_when_check_Tiposanguineo_of_paciente1() {
-            assertTrue(validUser.checkTiposanguineo("A+"));
-        }
-
+    void should_not_change_phone_when_invalid() {
+        validPaciente.setTelefone("96655-3322");
+        assertEquals("(32)96655-3322", validPaciente.getTelefone());
+    }
         @Test
-        void should_not_validate_Tiposanguineo_when_check_Tiposanguineo_of_paciente1() {
-            assertFalse(validUser.checkTiposanguineo("A+@"));
-        }
-
-        @Test
-        void should_not_validate_null_() {
-            assertFalse(validUser.checkTiposanguineo(null));
-        }
-
-        @Test
-        void should_not_validate_empty_() {
-            assertFalse(validUser.checkTiposanguineo(""));
-        }
-     }
+    void should_not_change_name_when_invalid() {
+        validPaciente.setNome("");
+        assertEquals(VALID_NAME, validPaciente.getNome());
+    }
+}
 }
