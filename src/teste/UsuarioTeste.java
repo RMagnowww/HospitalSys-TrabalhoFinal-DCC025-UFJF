3b4ca39588945ee9652110fa13043331f51fbe3f
Gabriel Angelo Bovareto Batista <gabrielbatista.bovareto@estudante.ufjf.br>
2026-01-09 03:27:09 -0300

    Modifica o Usuario Teste

--- src/teste/UsuarioTeste.java
+++ src/teste/UsuarioTeste.java
@@ -12,7 +12,7 @@ class UsuarioTest {
      private static final String VALID_PASSWORD = String.getInstance("paciente123");
      private static final String VALID_PHONE = String.getInstance("(32)96655-3322");
      private static final String VALID_EMAIL = String.getInstance("joao@paciente.com");
-     private static final PerfilUsuario VALID_PROFILE = PerfilUsuario.CLIENTE;
+     private static final PerfilUsuario VALID_PROFILE = PerfilUsuario.PACIENTE;
 
      private Usuario validUser;
      @BeforeEach
@@ -92,6 +92,98 @@ class UsuarioTest {
         void should_have_same_hashcode_when_users_are_equal() {
             Usuario user2 = new Usuario(VALID_NAME, VALID_CPF, VALID_PHONE, VALID_EMAIL, VALID_PASSWORD, VALID_PROFILE);
             assertEquals(validUser.hashCode(), user2.hashCode());
+        }
+    }
+         @Nested
+    @DisplayName("Name Tests")
+    class NameTests {
+        @Test
+        void should_validate_name_when_check_name_of_user1() {
+            assertTrue(validUser.checkName("Joao"));
+        }
+
+        @Test
+        void should_not_validate_name_when_check_name_of_user1() {
+            assertFalse(validUser.checkName("Joao,"));
+        }
+
+        @Test
+        void should_not_validate_null_name() {
+            assertFalse(validUser.checkName(null));
+        }
+
+        @Test
+        void should_not_validate_empty_name() {
+            assertFalse(validUser.checkName(""));
+        }
+    }
+         @Nested
+    @DisplayName("CPF Tests")
+    class CPFTests {
+        @Test
+        void should_validate_cpf_when_check_cpf_of_user1() {
+            assertTrue(validUser.checkcpf("086.756.245-90"));
+        }
+
+        @Test
+        void should_not_validate_cpf_when_check_cpf_of_user1() {
+            assertFalse(validUser.checkcpf("086.756.24-90"));
+        }
+
+        @Test
+        void should_not_validate_null_cpf() {
+            assertFalse(validUser.checkcpf(null));
+        }
+
+        @Test
+        void should_not_validate_empty_cpf() {
+            assertFalse(validUser.checkcpf(""));
+        }
+    }
+             @Nested
+    @DisplayName("Phone Tests")
+    class PhoneTests {
+        @Test
+        void should_validate_phone_when_check_phone_of_user1() {
+            assertTrue(validUser.checkPhone("(32)96655-3322"));
+        }
+
+        @Test
+        void should_not_validate_phone_when_check_phone_of_user1() {
+            assertFalse(validUser.checkPhone("96655-3322"));
+        }
+
+        @Test
+        void should_not_validate_null_phone() {
+            assertFalse(validUser.checkPhone(null));
+        }
+
+        @Test
+        void should_not_validate_empty_phone() {
+            assertFalse(validUser.checkPhone(""));
+        }
+    }
+             @Nested
+    @DisplayName("Email Tests")
+    class EmailTests {
+        @Test
+        void should_validate_Email_when_check_Email_of_user1() {
+            assertTrue(validUser.checkemail("joao@paciente.com"));
+        }
+
+        @Test
+        void should_not_validate_Email_when_check_Email_of_user1() {
+            assertFalse(validUser.checkemail("joao@paciente.com."));
+        }
+
+        @Test
+        void should_not_validate_null_Email() {
+            assertFalse(validUser.checkemail(null));
+        }
+
+        @Test
+        void should_not_validate_empty_Email() {
+            assertFalse(validUser.checkemail(""));
         }
     }
 }
